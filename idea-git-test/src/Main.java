b280a5722b7ba7c531a3b85c947a245c3d67466a
moka <471829304@qq.com>
2019-03-25 17:31:52 +0800

    Revert "添加say方法"
    This reverts commit e80adb9b9fa8bfed472c28d9f41e21b485e67f36.

--- idea-git-test/src/Main.java
+++ idea-git-test/src/Main.java
@@ -3,8 +3,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    
-    public void say(){
-        System.out.println("hello");
-    }
 }
